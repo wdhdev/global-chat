3cc17303145587a68c21908f70c348fcc66bd592
William Harrison <william@williamdavidharrison.com.au>
2023-06-05 20:13:57 +0800

    fix: member undefined

--- src/util/getRoles.js
+++ src/util/getRoles.js
@@ -5,7 +5,7 @@ module.exports = async function (message, client) {
     const role = user.roles;
 
     const guild = await client.guilds.fetch(client.config_default.server);
-    const member = await guild.members.fetch(message.author);
+    const member = await guild.members.fetch(message.author) || null;
 
     const owner = client.config_default.owner === message.author.id;
     const supporter = member.premiumSinceTimestamp ? true : false;
@@ -26,7 +26,7 @@ module.exports.interaction = async function (interaction, client) {
     const role = user.roles;
 
     const guild = await client.guilds.fetch(client.config_default.server);
-    const member = await guild.members.fetch(interaction.user);
+    const member = await guild.members.fetch(interaction.user) || null;
 
     const owner = client.config_default.owner === interaction.user.id;
     const supporter = member.premiumSinceTimestamp ? true : false;
