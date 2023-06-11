e94648ce96b9d8cc3d4ca69495c821c719227e42
William Harrison <william@williamdavidharrison.com.au>
2023-06-11 13:32:57 +0800

    feat: cleanup database on ready

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,7 +3,7 @@ const bannedGuildSchema = require("../../models/bannedGuildSchema");
 module.exports = {
 	name: "ready",
 	once: true,
-	async execute(client) {
+	async execute(client, Discord) {
         try {
 			// Login Message
 			console.log(`Logged in as: ${client.user.tag.endsWith("#0") ? `@${client.user.username}` : client.user.tag}`);
@@ -16,6 +16,58 @@ module.exports = {
 			client.guilds.cache.forEach(async guild => {
 				if(await bannedGuildSchema.exists({ _id: guild.id })) guild.leave();
 			})
+
+			// Cleanup Database
+            const logsChannel = client.channels.cache.get(client.config_channels.logs);
+
+			// Banned Guilds
+			const cleanBannedGuilds = require("../../util/database/cleanBannedGuilds");
+			const bannedGuildsRes = await cleanBannedGuilds(client);
+
+			const bannedGuildsResult = new Discord.EmbedBuilder()
+				.setColor(client.config_embeds.default)
+				.setAuthor({ name: client.user.tag.endsWith("#0") ? `@${client.user.username}` : client.user.tag, iconURL: client.user.displayAvatarURL({ format: "png", dynamic: true }), url: `https://discord.com/users/${client.user.id}` })
+				.setTitle("🧹 Collection Cleanup")
+				.setDescription("**Collection**: `banned-guilds`")
+				.addFields (
+					{ name: "🗑️ Removed Documents", value: bannedGuildsRes.removed.length ? `\`\`\`${bannedGuildsRes.removed.join("\n")}\`\`\`` : "*None*" }
+				)
+				.setTimestamp()
+
+			logsChannel.send({ embeds: [bannedGuildsResult] });
+
+			// Banned Users
+			const cleanBannedUsers = require("../../util/database/cleanBannedUsers");
+			const bannedUsersRes = await cleanBannedUsers(client);
+
+			const bannedUsersResult = new Discord.EmbedBuilder()
+				.setColor(client.config_embeds.default)
+				.setAuthor({ name: client.user.tag.endsWith("#0") ? `@${client.user.username}` : client.user.tag, iconURL: client.user.displayAvatarURL({ format: "png", dynamic: true }), url: `https://discord.com/users/${client.user.id}` })
+				.setTitle("🧹 Collection Cleanup")
+				.setDescription("**Collection**: `banned-users`")
+				.addFields (
+					{ name: "🗑️ Removed Documents", value: bannedUsersRes.removed.length ? `\`\`\`${bannedUsersRes.removed.join("\n")}\`\`\`` : "*None*" }
+				)
+				.setTimestamp()
+
+			logsChannel.send({ embeds: [bannedUsersResult] });
+
+			// Channels
+			const cleanChannels = require("../../util/database/cleanChannels");
+			const channelsRes = await cleanChannels(client);
+
+			const channelsResult = new Discord.EmbedBuilder()
+				.setColor(client.config_embeds.default)
+				.setAuthor({ name: client.user.tag.endsWith("#0") ? `@${client.user.username}` : client.user.tag, iconURL: client.user.displayAvatarURL({ format: "png", dynamic: true }), url: `https://discord.com/users/${client.user.id}` })
+				.setTitle("🧹 Collection Cleanup")
+				.setDescription("**Collection**: `channels`")
+				.addFields (
+					{ name: "📝 Modified Documents", value: channelsRes.modified.length ? `\`\`\`${channelsRes.modified.join("\n")}\`\`\`` : "*None*" },
+					{ name: "🗑️ Removed Documents", value: channelsRes.removed.length ? `\`\`\`${channelsRes.removed.join("\n")}\`\`\`` : "*None*" }
+				)
+				.setTimestamp()
+
+			logsChannel.send({ embeds: [channelsResult] });
 		} catch(err) {
 			client.logEventError(err);
 		}
