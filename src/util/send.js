46a765c426dda23ded43a4f7954fe892e84d4dbd
William Harrison <william@williamdavidharrison.com.au>
2023-06-18 18:10:13 +0800

    chore: add emoji to banned user blocked msg

--- src/util/send.js
+++ src/util/send.js
@@ -30,7 +30,7 @@ module.exports = async function (message, client, Discord) {
         const blocked = new Discord.EmbedBuilder()
             .setAuthor({ name: message.author.tag.endsWith("#0") ? `@${message.author.username}` : message.author.tag, iconURL: message.author.displayAvatarURL({ format: "png", dynamic: true }), url: `https://discord.com/users/${message.author.id}` })
             .addFields (
-                { name: "Reason", value: "Banned User" }
+                { name: "❓ Reason", value: "Banned User" }
             )
             .setTimestamp()
 
