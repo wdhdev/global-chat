9ba390fe8f1bd8fef4075fdcde674401d1e23354
William Harrison <william@williamdavidharrison.com.au>
2023-06-24 13:57:49 +0800

    feat: re-create /filter list

--- src/commands/filter.js
+++ src/commands/filter.js
@@ -28,15 +28,42 @@ module.exports = {
                     description: "The filter you want to add the word to.",
                     choices: [
                         {
-                            name: "block",
-                            description: "Add the word to the blocked words filter.",
-                            value: "block"
-                        },
-
-                        {
                             name: "autoban",
                             description: "Add the word to the autoban filter.",
                             value: "autoban"
+                        },
+
+                        {
+                            name: "block",
+                            description: "Add the word to the blocked words filter.",
+                            value: "block"
+                        }
+                    ],
+                    required: true
+                }
+            ]
+        },
+
+        {
+            type: 1,
+            name: "list",
+            description: "Get a list of the words on a filter.",
+            options: [
+                {
+                    type: 3,
+                    name: "filter",
+                    description: "The filter you want a list of words from.",
+                    choices: [
+                        {
+                            name: "autoban",
+                            description: "Get all words on the autoban filter.",
+                            value: "autoban"
+                        },
+
+                        {
+                            name: "block",
+                            description: "Get all words on the blocked words filter.",
+                            value: "block"
                         }
                     ],
                     required: true
@@ -64,15 +91,15 @@ module.exports = {
                     description: "The filter you want to remove the word from.",
                     choices: [
                         {
-                            name: "block",
-                            description: "Remove the word from the blocked words filter.",
-                            value: "block"
-                        },
-
-                        {
                             name: "autoban",
                             description: "Remove the word to from autoban filter.",
                             value: "autoban"
+                        },
+
+                        {
+                            name: "block",
+                            description: "Remove the word from the blocked words filter.",
+                            value: "block"
                         }
                     ],
                     required: true
@@ -141,6 +168,52 @@ module.exports = {
                     }
                 })
 
+                return;
+            }
+
+            if(interaction.options.getSubcommand() === "list") {
+                const filter = interaction.options.getString("filter");
+
+                const data = await filterSchema.findOne({ _id: filter }) || { words: [] };
+
+                if(!data.words.length) {
+                    const error = new Discord.EmbedBuilder()
+                        .setColor(client.config_embeds.error)
+                        .setDescription(`${emoji.error} There are no words on the filter!`)
+
+                    await interaction.editReply({ embeds: [error], ephemeral: true });
+                    return;
+                }
+
+                const filters = {
+                    autoban: "Autoban",
+                    block: "Block"
+                }
+
+                const embed = new Discord.EmbedBuilder()
+                    .setColor(client.config_embeds.default)
+                    .setTitle(`${filters[filter]} Filter`)
+                    .setDescription(`\`${data.words.sort().join("\`, \`")}\``)
+
+                let msgURL = null;
+
+                try {
+                    await interaction.user.send({ embeds: [embed] })
+                        .then(msg => msgURL = `https://discord.com/channels/@me/${msg.channelId}/${msg.id}`)
+                } catch {
+                    const error = new Discord.EmbedBuilder()
+                        .setColor(client.config_embeds.error)
+                        .setDescription(`${emoji.error} I could not DM you!`)
+
+                    await interaction.editReply({ embeds: [error], ephemeral: true });
+                    return;
+                }
+
+                const sent = new Discord.EmbedBuilder()
+                    .setColor(client.config_embeds.default)
+                    .setDescription(`💬 ${msgURL}`)
+
+                await interaction.editReply({ embeds: [sent] });
                 return;
             }
 
--- src/util/filter/filters/profranity.js
+++ src/util/filter/filters/profranity.js
@@ -2,23 +2,23 @@ module.exports = async function profanity(message) {
     const filterSchema = require("../../../models/filterSchema");
     const replaceContent = require("../replaceContent");
 
-    const blockFilter = await filterSchema.findOne({ _id: "block" }) || { words: [] };
     const autobanFilter = await filterSchema.findOne({ _id: "autoban" }) || { words: [] };
+    const blockFilter = await filterSchema.findOne({ _id: "block" }) || { words: [] };
 
     const content = replaceContent(message.content.toLowerCase());
 
     const blockedWords = [];
     let autoban = false;
 
-    blockFilter.words.some(word => {
-        if(content.includes(word)) blockedWords.push(word);
-    })
-
     autobanFilter.words.some(word => {
         if(content.includes(word)) {
             blockedWords.push(word);
             autoban = true;
         }
+    })
+
+    blockFilter.words.some(word => {
+        if(content.includes(word)) blockedWords.push(word);
     })
 
     if(blockedWords.length) {
