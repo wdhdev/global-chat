8f5fefbda291c0900c9ccfb22a32d35b5a923fb4
William Harrison <william@williamdavidharrison.com.au>
2023-06-30 06:59:01 +0800

    Update send.js

--- src/util/send.js
+++ src/util/send.js
@@ -33,14 +33,6 @@ module.exports = async function (message, client, Discord) {
             )
             .setTimestamp()
 
-        const actions = new Discord.ActionRowBuilder()
-            .addComponents (
-                new Discord.ButtonBuilder()
-                    .setStyle(Discord.ButtonStyle.Secondary)
-                    .setCustomId(`blocked-message-info-${message.id}`)
-                    .setEmoji("ℹ️")
-            )
-
         if(message.content.length) blocked.setDescription(message.content);
 
         if(message.attachments.first()) {
@@ -56,7 +48,7 @@ module.exports = async function (message, client, Discord) {
             }
         }
 
-        blockedChannel.send({ embeds: [blocked], components: [actions] });
+        blockedChannel.send({ embeds: [blocked] });
         return;
     }
 
