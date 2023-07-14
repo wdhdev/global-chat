80ba6732fc7bc04eae7bf345fea2e0ef8183b96d
William Harrison <william@williamdavidharrison.com.au>
2023-07-15 07:40:37 +0800

    Update easter-eggs.js

--- src/util/easter-eggs.js
+++ src/util/easter-eggs.js
@@ -1,7 +1,22 @@
 module.exports = async function (content) {
+    // andrew w
+    if(content.toLowerCase() === "andrew w") {
+        return "Andrew is the best developer 💯";
+    }
+
+    // ethereal
+    if(content.toLowerCase() === "ethereal") {
+        return "Ethereal should get $10";
+    }
+
     // fml
     if(content.toLowerCase() === "fml") {
         return "Free my life, free my losses! I want to live!";
+    }
+
+    // help me
+    if(content.toLowerCase() === "help me") {
+        return "William is keeping me in his basement and is feeding me strawberries";
     }
 
     // kys
@@ -9,9 +24,19 @@ module.exports = async function (content) {
         return "I love you so much and I wish you the best in life! 🥰";
     }
 
+    // luni better
+    if(content.toLowerCase() === "luni better") {
+        return "Luni = best moderator";
+    }
+
     // OwO
     if(content === "OwO") {
         return "OwO What's This?";
+    }
+
+    // the duck song
+    if(content.toLowerCase() === "the duck song") {
+        return "A duck walked up to a lemonade stand and he said to the man running the stand \"Hey! Got any grapes?\"";
     }
 
     // uwu
