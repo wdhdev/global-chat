9f3bed3ab4b253f625f3208de69e06e357caeb9a
William Harrison <william@williamdavidharrison.com.au>
2023-07-22 08:21:18 +0800

    fix: links filter

--- src/util/filter/filters/links.ts
+++ src/util/filter/filters/links.ts
@@ -4,7 +4,7 @@ import { Message } from "discord.js";
 export default async function (message: Message, role: Roles) {
     const regex = new RegExp(/(?:https?:\/\/)[a-z0-9_\-\.]*[a-z0-9_\-]{2,}\.[a-z]{2,}/g);
 
-    const matches = message.content.match(regex);
+    const matches = message.content.match(regex) || [];
 
     if(matches.length && !role.verified) {
         return {
