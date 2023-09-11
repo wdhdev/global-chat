bb3ef96fc52db127ced2f2b94e7f0dda1d319cdf
andrewstech <andrew@win11react.com>
2023-09-11 21:38:15 +0000

    fix

--- src/commands/mod/kill.ts
+++ src/commands/mod/kill.ts
@@ -2,23 +2,24 @@ import Command from "../../classes/Command";
 import ExtendedClient from "../../classes/ExtendedClient";
 import { CommandInteraction } from "discord.js";
 import killswitch from "../../models/killswitch";
-
 import { announce } from "../../util/send";
+
+
 import { emojis as emoji } from "../../config";
 
 const command: Command = {
-    name: "killswitch",
-    description: "[STAFF ONLY] Emergency killswitch.",
+    name: "kill",
+    description: "Killswitch.",
     options: [],
     default_member_permissions: null,
     botPermissions: [],
     requiredRoles: ["mod"],
-    cooldown: 60,
+    cooldown: 10,
     enabled: true,
     allowWhileBanned: false,
-    guildOnly: true,
+    guildOnly: false,
     deferReply: false,
-    ephemeral: true,
+    ephemeral: false,
     async execute(interaction: CommandInteraction, client: ExtendedClient, Discord: any) {
         try {
             const killData = await killswitch.findOne({ killswitch: true });
@@ -26,14 +27,25 @@ const command: Command = {
                 await killswitch.findOneAndUpdate({ killswitch: false });
                 const text = `${emoji.tick} The killswitch has been deactivated.`
                 await announce(text, interaction, client, Discord);
+                const killswitchMessage = new Discord.EmbedBuilder()
+                    .setColor(client.config_embeds.default)
+                    .addFields (
+                        { name: "KillSwitch", value: 'Deactivated', inline: true }
+                    )
+                await interaction.reply({ embeds: [killswitchMessage] });
                 return;
             } else {
                 (await killswitch.create({ killswitch: true })).save();
                 const text = `${emoji.tick} The killswitch has been activated.`
                 await announce(text, interaction, client, Discord);
+                const killswitchMessage = new Discord.EmbedBuilder()
+                    .setColor(client.config_embeds.default)
+                    .addFields (
+                        { name: "KillSwitch", value: 'Activated', inline: true }
+                    )
+                await interaction.reply({ embeds: [killswitchMessage] });
                 return;
             }
-            
         } catch(err) {
             client.logCommandError(err, interaction, Discord);
         }
