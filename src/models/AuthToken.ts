9bdc653bbe1b0cf4cbef6c815f2a4f32d12d9673
William Harrison <william@williamdavidharrison.com.au>
2024-02-17 17:33:59 +0800

    Delete AuthToken.ts

--- src/models/AuthToken.ts
+++
@@ -1,9 +0,0 @@
-import { model, Schema } from "mongoose";
-
-const schema = new Schema({
-    _id: String,
-    user: String,
-    service: String
-})
-
-export default model("auth-tokens", schema, "auth-tokens");
